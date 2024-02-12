134479adf48efc270eb82cfd75ffccd3619a9581
lxndrbukin <alex.bukin@hotmail.com>
2024-02-12 14:31:05 +0000

    Working on the chat functionality

--- src/client/src/components/Rooms/RoomChat.tsx
+++ src/client/src/components/Rooms/RoomChat.tsx
@@ -1,50 +1,76 @@
-import { FC, FormEvent, useRef, useContext } from 'react';
+import {
+  FC,
+  FormEvent,
+  KeyboardEvent,
+  ChangeEvent,
+  useRef,
+  useState,
+  useContext,
+} from 'react';
 import { RoomChatProps } from './types';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { SocketContext } from '../../context/SocketProvider';
 import { LuSendHorizonal } from 'react-icons/lu';
 import { RoomChatMsg } from './RoomChatMsg';
-import { RootState } from '../../store';
+import { RoomMessage, RootState } from '../../store';
 
 export const RoomChat: FC<RoomChatProps> = ({ messages }): JSX.Element => {
-  const formRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLDivElement>(null);
   const webSocket = useContext(SocketContext);
+  const [inputText, setInputText] = useState('');
   const { userData } = useSelector((state: RootState) => state.session);
   const { currentRoom } = useSelector((state: RootState) => state.rooms);
   const { roomId } = useParams();
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const target = e.target as typeof e.target & {
-      chatMsg: { value: string };
-    };
-    const { chatMsg } = target;
-    console.log(chatMsg.value);
+    console.log(inputRef.current?.innerHTML);
     const msgData = JSON.stringify({
       type: 'message',
       roomId,
-      text: chatMsg.value,
+      text: inputRef.current?.innerHTML,
       username: userData?.username,
     });
     webSocket.send(msgData);
   };
 
-  const renderMessages = currentRoom?.messages?.map((message, idx) => {
-    return <RoomChatMsg key={idx} {...message} />;
-  });
+  const handleKeyDown = (e: KeyboardEvent | FormEvent): void => {
+    if (
+      (e as KeyboardEvent).key === 'Enter' &&
+      !(e as KeyboardEvent).shiftKey
+    ) {
+      handleSubmit(e as FormEvent<HTMLFormElement>);
+      inputRef.current!.innerHTML = '';
+    } else {
+      setInputText(inputRef.current?.innerHTML!);
+    }
+  };
+
+  const placeholder = (
+    <span className='room-chat-input-placeholder'>Message</span>
+  );
+
+  const renderMessages = currentRoom?.messages?.map(
+    (message: RoomMessage, idx: number): JSX.Element => {
+      return <RoomChatMsg key={idx} {...message} />;
+    }
+  );
 
   return (
-    <div className="room-chat-container">
-      <div className="room-chat">
-        <ul className="room-chat-messages">{renderMessages}</ul>
-        <form ref={formRef} onSubmit={handleSubmit} className="room-chat-form">
-          <input
-            className="room-chat-input"
-            placeholder="Write a message..."
-            name="chatMsg"
-          />
-          <button className="room-chat-send">
+    <div className='room-chat-container'>
+      <div className='room-chat'>
+        <ul className='room-chat-messages'>{renderMessages}</ul>
+        <form onSubmit={handleSubmit} className='room-chat-form'>
+          <div
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
+            className='room-chat-input'
+            contentEditable
+            suppressContentEditableWarning
+          ></div>
+          {!inputRef.current?.innerText && placeholder}
+          <button className='room-chat-send'>
             <LuSendHorizonal size={30} />
           </button>
         </form>
